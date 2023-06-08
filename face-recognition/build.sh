2d68546e03b016709e546894ecb4c915545b05a8
salamer <salamer_gaga@163.com>
2023-06-09 01:51:24 +0800

    update: add upsert method

--- face-recognition/build.sh
+++ face-recognition/build.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+yum -y update
+yum -y install gcc make git build-essential g++ clang
+
 git clone https://github.com/davisking/dlib.git
 cd dlib
 mkdir build; cd build; cmake ..; cmake --build .
