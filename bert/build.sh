d2fd7610025d7dc251d0c7fd1349b8eb36dce438
salamer <salamer_gaga@163.com>
2023-06-09 21:28:35 +0800

    update: add upsert method

--- bert/build.sh
+++ bert/build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 pip3 install --upgrade pip cmake
 yum -y update
-yum -y install gcc make git build-essential g++ clang
+yum -y install gcc make git build-essential g++ clang git-lfs
 git lfs install
 git clone git@hf.co:csebuetnlp/mT5_multilingual_XLSum
