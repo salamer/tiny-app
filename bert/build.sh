732351c90ee2029e13e8e8549a50ad9fb5f7a1a3
salamer <salamer_gaga@163.com>
2023-06-10 00:10:27 +0800

    update: add upsert method

--- bert/build.sh
+++ bert/build.sh
@@ -2,6 +2,8 @@
 pip3 install --upgrade pip cmake
 yum -y update
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | bash
-yum -y install gcc make git build-essential g++ clang git-lfs
+yum -y install gcc make git build-essential g++ clang git-lfs amazon-linux-extras
+amazon-linux-extras install epel -y 
+yum-config-manager --enable epel
 git lfs install
 git clone git@hf.co:csebuetnlp/mT5_multilingual_XLSum
