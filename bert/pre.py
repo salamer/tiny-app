65b97099daaaeb088751031a4a32a128cc31d3bd
salamer <salamer_gaga@163.com>
2023-06-10 01:43:51 +0800

    update: add upsert method

--- bert/pre.py
+++ bert/pre.py
@@ -1,4 +1,4 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-model_name = "csebuetnlp/mT5_multilingual_XLSum"
+model_name = "ainize/bart-base-cnn"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
