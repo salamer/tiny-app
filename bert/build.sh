054296c089ae7d8f499ebec7ece3d1493ad8b42b
salamer <salamer_gaga@163.com>
2023-06-10 01:23:08 +0800

    update: add upsert method

--- bert/build.sh
+++ bert/build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-pip3 install --upgrade pip cmake transformers sentencepiece protobuf==3.20.*
+pip3 install --upgrade pip cmake transformers sentencepiece torch torchvision torchaudio protobuf==3.20.*
 yum -y update
 yum -y install gcc make git build-essential g++ clang
 python pre.py
