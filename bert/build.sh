b602ebff41dfe8322bf9d2a4b3ee031d8b44f947
salamer <salamer_gaga@163.com>
2023-06-10 01:19:24 +0800

    update: add upsert method

--- bert/build.sh
+++ bert/build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-pip3 install --upgrade pip cmake transformers sentencepiece protobuf
+pip3 install --upgrade pip cmake transformers sentencepiece protobuf==3.20.*
 yum -y update
 yum -y install gcc make git build-essential g++ clang
 python pre.py
--- whisper/app.py
+++ whisper/app.py
@@ -7,9 +7,14 @@ import jax.numpy as jnp
 pipeline = FlaxWhisperPipline("openai/whisper-large-v2", dtype=jnp.bfloat16)
 
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 
 app = Flask(__name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def hello():
@@ -18,6 +23,38 @@ def hello():
 @app.route("/xx")
 def xx():
     return {"qq":"xx"}
+
+
+def handle_audio(file_stream):
+    audio = pipeline(file_stream)
+    return audio
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    # Check if a valid image file was uploaded
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # The image file seems valid! Detect faces and return the result.
+            return detect_faces_in_image(file)
+
+    # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method="POST" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="Upload">
+    </form>
+    '''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888, debug=True)
