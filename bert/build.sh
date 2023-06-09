90e907fe59ee1099216b26ce09b1c0458e091c48
salamer <salamer_gaga@163.com>
2023-06-09 21:59:24 +0800

    update: add upsert method

--- bert/build.sh
+++ bert/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 pip3 install --upgrade pip cmake
 yum -y update
-yum -y install epel-release
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install gcc make git build-essential g++ clang git-lfs
 git lfs install
 git clone git@hf.co:csebuetnlp/mT5_multilingual_XLSum
